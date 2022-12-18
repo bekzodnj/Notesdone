53d6ee6683baf73e4781049d02655db0699930ed
Bekzodjon Norkuziev <bekzodnx@gmail.com>
2022-12-18 17:42:45 +0100

    init layout for web app

---
+++ components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+export function Navbar() {
+  return (
+    <div className='py-5 px-9 shadow-md text-lg flex justify-between'>
+      <div id='logo'>
+        <Link href='/'>Notesdone</Link>
+      </div>
+
+      <div id='menu'>
+        <ul className='flex space-x-5'>
+          <li>
+            <Link href='/'>Templates</Link>
+          </li>
+          <li>
+            <Link href='/'>Build</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
